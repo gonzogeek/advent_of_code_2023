cf16a69fddaa9464a487fbf741aaa8e7e2c1124d
gonzogeek <pellcooper@gmail.com>
2023-12-12 23:10:11 -0800

    oops I did it again

--- Day03/day_03.rb
+++ Day03/day_03.rb
@@ -6,7 +6,6 @@ module Day03
   # numbers match
   
   def part1(file)
-    # frozen_string_literal: true
 
     s = File.new(file).readlines(chomp: true).map { |l| ".#{l}." }
 
@@ -30,3 +29,24 @@ module Day03
     puts ans
     ans
   end
+
+  def part2(file)
+    s = File.new(file).readlines(chomp: true).map { |l| ".#{l}." }
+
+    n = s.length
+    s.prepend '.' * (n + 2)
+    s.append  '.' * (n + 2)
+
+    nums = s.map { |line| line.gsub(/.(\d+)/).map { [Regexp.last_match(1).to_i, Range.new(*Regexp.last_match.offset(0))] } }
+
+    ans = 1.upto(n).sum do |i|
+      s[i].gsub(/\*/).sum do
+        gear_index = Regexp.last_match.begin(0)
+        adjacent_parts = nums[(i - 1)..(i + 1)].flat_map { |p| p.select { |_, r| r.cover?(gear_index) } }.map(&:first)
+        adjacent_parts.length == 2 ? adjacent_parts.inject(&:*) : 0
+      end
+    end
+
+    puts ans
+  end
+end
